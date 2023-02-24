fcbda33f7f7d4c8c861e9c906a79489b40d10f7a
unknown <trunghieu210904@gmail.com>
2023-02-24 19:01:39 +0700

    toan tu gan

--- base.js
+++ base.js
@@ -13,8 +13,17 @@ console.log(fullName)
 confirm('ban co du tuoi?')
 prompt('nhap so tuoi')
 setTimeout(() => {
-    alert('alert sau 5 giay')
+    alert('thuc hien sau 5 giay')
 }, 5000);
 setInterval(() => {
     console.log('lap lai trong log sau 2 giay')
 }, 2000);
+// toán tử gán 
+var a = 1
+a += 2
+console.log(a)
+// tương đương a = a + 2
+a -= 2
+// tương đương a = a - 2
+// *=, /=, **= tương tự
+

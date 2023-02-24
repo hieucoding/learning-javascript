e260f1c6e061f602cc7e514c9895b7eb3b208fe6
unknown <trunghieu210904@gmail.com>
2023-02-24 17:34:18 +0700

    mot so ham built-in

--- base.js
+++ base.js
@@ -9,3 +9,12 @@ abc
 alert(fullName)
 alert(height)
 alert(age)
+console.log(fullName)
+confirm('ban co du tuoi?')
+prompt('nhap so tuoi')
+setTimeout(() => {
+    alert('alert sau 5 giay')
+}, 5000);
+setInterval(() => {
+    console.log('lap lai trong log sau 2 giay')
+}, 2000);

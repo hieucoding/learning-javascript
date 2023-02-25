d279a431c4a91cf518a8792a38d89927ee467cd9
unknown <trunghieu210904@gmail.com>
2023-02-25 19:22:28 +0700

    === and !==

--- base.js
+++ base.js
@@ -1,28 +1,4 @@
-// number type
 var a = 1
-var b = 2
-var c = 3
-//string type
-var fullName = 'Duong Hieu'
-// boolean
-var isSuccess = true
-//undefined (biến 0 đc gán)
-var age
-//null (nothing/ 0 có gì)
-var isNull = null
-// Nan (số không xác định được)
-var d = 10
-var e = 0
-f = d / e
-console.log(f)
-// object
-var object = {
-    name: 'Hieu',
-    age: '19',
-    adress: 'Hanoi'
-}
-console.log(object)
-// symbol
-var id = Symbol('id') //unique
-var id2 = Symbol('id') //unique
-console.log(id === id2)
+var  b = '1'
+console.log(a === b)
+console.log(a !== b)

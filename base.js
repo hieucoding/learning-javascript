ba9edb4d86797bc4e5482ab9a6ffdc5cd6ad5a67
unknown <trunghieu210904@gmail.com>
2023-02-25 16:18:21 +0700

    toan tu logic

--- base.js
+++ base.js
@@ -1,7 +1,18 @@
-var a = 10
-var b = 5
-if (a > b) {
-    console.log('true')
+var a = 1
+var b = 2
+var c = 3
+if (a > 0 && b > 0 ) {
+    console.log('dieu kien dung')
 } else {
-    console.log('false')
+    console.log('dieu kien sai')
+}
+if (a < 0 || b > 0 ) {
+    console.log('dieu kien dung')
+} else {
+    console.log('dieu kien sai')
+}
+if (!(a < 0)) {
+    console.log('dieu kien dung')
+} else {
+    console.log('dieu kien sai')
 }

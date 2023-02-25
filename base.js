5a86154fdcfeff0bcb70224f01c4ac078ddb303f
unknown <trunghieu210904@gmail.com>
2023-02-25 11:38:56 +0700

    gioi thieu boolean

--- base.js
+++ base.js
@@ -1,17 +1,4 @@
-
-// toan tu so sanh
-/*
-==          --> bang
-!=          --> ko bang
-<=          --> nho hon hoac bang
->=          --> lon hon hoac bang
-<           --> nho hon
->           --> lon hon
-*/
 var a = 1
 var b = 2
-if (a < b) {
-    console.log('dieu kien dung');
-} else {
-    console.log('dieu kien sai');
-}
+var isSuccess = a > b
+console.log(isSuccess)

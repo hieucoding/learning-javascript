ed1d6d7d12f94bd89c66e7684f7877b1b3d5172a
unknown <trunghieu210904@gmail.com>
2023-02-26 18:15:53 +0700

    break & continue

--- base.js
+++ base.js
@@ -1,34 +1,12 @@
-// loop for
-var myArray = [
-    'duong',
-    'trung',
-    'hieu',
-    '1234',
-]
-var arrayLenght = myArray.length
-for (i = 0; i < arrayLenght; i++) [
-    console.log(myArray[i])
-]
-// loop for/in
-var myInfo = {
-    name: 'duong hieu',
-    age: '19',
+for (var a = 0; a < 10; a++) {
+    console.log(a)
+    if (a >= 5) {
+        break
+    }
 }
-for (var key in myInfo) {
-    console.log(myInfo[key])
-}
-// loop for/of
-var myArray = [
-    'name',
-    'age',
-    'number'
-]
-for (var value of myArray) {
-    console.log(value)
-}
-// loop do/while
-var a = 0
-do {
-    a++
-    console.log (a)
-} while (a < 10)
+for (var a = 0; a < 10; a++){
+    if (a%2 !== 0) {
+        continue
+    }
+    console.log(a)
+}    

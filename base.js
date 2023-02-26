88e0914e5dd25216047ddb8e0335d1fb98022915
unknown <trunghieu210904@gmail.com>
2023-02-26 18:02:38 +0700

    loop

--- base.js
+++ base.js
@@ -1,4 +1,34 @@
-var a = 1
-var  b = '1'
-console.log(a === b)
-console.log(a !== b)
+// loop for
+var myArray = [
+    'duong',
+    'trung',
+    'hieu',
+    '1234',
+]
+var arrayLenght = myArray.length
+for (i = 0; i < arrayLenght; i++) [
+    console.log(myArray[i])
+]
+// loop for/in
+var myInfo = {
+    name: 'duong hieu',
+    age: '19',
+}
+for (var key in myInfo) {
+    console.log(myInfo[key])
+}
+// loop for/of
+var myArray = [
+    'name',
+    'age',
+    'number'
+]
+for (var value of myArray) {
+    console.log(value)
+}
+// loop do/while
+var a = 0
+do {
+    a++
+    console.log (a)
+} while (a < 10)

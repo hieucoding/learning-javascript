46264e65993cd18ff6a8d9773815b37bf47af90b
unknown <trunghieu210904@gmail.com>
2023-03-01 22:17:26 +0700

    tim nam nhuan

--- base.js
+++ base.js
@@ -1,12 +1,23 @@
-function user(firstName, lastName, avatar ) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.avatar = avatar
+function leapYearRange(start, end) {
+    var yearRange = []
+    for (var i = start; i <= end; i++)
+{
+    yearRange.push(i)
 }
-
-user.prototype.className = '1a'
-
-var newUser = new user('hieu', 'duong', 'avatar')
-
-console.log(newUser)
-console.log(newUser.className)
+var new_array = []
+yearRange.forEach(
+    function(year)
+    {
+        if (findYear(year))
+        new_array.push(year)
+    })
+    return new_array
+}    
+function findYear(year) {
+    if ((year%4 ==0 && year%100 !=0 && year% 400 !=0)||(year%100 ==0 && year%400)) {
+        return year
+    } else {
+        return false
+    }
+}
+console.log (leapYearRange(2000, 2020))

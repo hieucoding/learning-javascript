3ce943fe64e089a0db5ffb8307637cba192db9f5
unknown <trunghieu210904@gmail.com>
2023-03-02 21:33:13 +0700

    sua lai toan tu

--- base.js
+++ base.js
@@ -9,16 +9,24 @@ for (let i = 1; i < 5; i++) {
   c = c++;
   // vong lap 1
   // i = 1
-  // a = 
-  // c =
+  // a = 7 
+  // c = 2 // CV1: c = c (c = 2), CV2: c= c + 1 (c = 3)
   // vong lap 2
-  // i = 
-  // a =
-  // c =
+  // i = 2
+  // a = 12
+  // c = 3 // CV1: c = c (c = 3), CV2: c = c + 1 (c = 4)
+  // vong lap 3
+  // i = 3
+  // a = 17 
+  // c = 4 // CV1: c = c (c = 4), CV2: c = c + 1 (c = 5)
+  // vong lap 4
+  //i = 4
+  // a = 22
+  // c = 5 // CV1: c = c (c = 5), CV2: c = c + 1 (c = 6)
   console.log(i)
   console.log(a)
   console.log(c)
 }
-let d = ++a; 
-let e = --a;
-let f = a--;
+let d = ++a; // d = 23 // CV1: a = a + 1(a = 23), CV2: d = a (d = 23, a = 23)  
+let e = --a; // e = 22 // CV1: a = a - 1(a = 22), CV2: e = a (e = 22, a = 22)
+let f = a--;// f = 22 // CV1: f = a (a = 22, f = 22), CV2: a = a - 1 (a = 21)

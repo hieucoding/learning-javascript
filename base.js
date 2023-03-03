9f69181acf0ce0341084befb1b667d24f9f38110
unknown <trunghieu210904@gmail.com>
2023-03-03 19:57:07 +0700

    array methods

--- base.js
+++ base.js
@@ -1,11 +1,51 @@
-var myArray = [
-  [1, 2],
-  [3, 4],
-  [5, 6],
-  [7, 8]
+var personID =  [
+  {
+    id: 1,
+    name: 'duong',
+    age: 18
+  },
+  {
+    id: 2,
+    name: 'trung',
+    age: 19
+  },
+  {
+    id: 3,
+    name: 'hieu',
+    age: 20
+  },
+  {
+    id: 4,
+    name: 'dai',
+    age: 22
+  },
+  {
+    id: 5,
+    name: 'nghia',
+    age: 40
+  }
 ]
-for (var i = 0; i < myArray.length; i++) {
-    for (var j = 0; j < myArray[i].length; j++) {
-      console.log(myArray[i][j])
-    }  
-} 
+
+personID.forEach(function(person) {
+  console.log(person)
+})
+
+var ageFind = personID.every(function(person) {
+  return person.age < 20
+})
+console.log(ageFind)
+
+var ageFind = personID.some(function(person) {
+  return person.age < 20
+})
+console.log(ageFind)
+
+var ageFind = personID.find(function(person) {
+  return person.name = "duong"
+})
+console.log(ageFind)
+
+var ageFind = personID.filter(function(person) {
+  return person.age < 20
+})
+console.log(ageFind)

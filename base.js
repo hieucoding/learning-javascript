36926bb3ef25980b3769e0cf5166a51f3e1aaf9e
unknown <trunghieu210904@gmail.com>
2023-03-03 17:50:52 +0700

    lenh re nhanh switch

--- base.js
+++ base.js
@@ -1,8 +1,14 @@
-var a = 10
-if (a == 5) {
-  console.log('a = 5')
-} else if (a == 10) {
-  console.log('a = 10')
-} else if (a == 15) {
-  console.log('a = 15')
+var date = 5
+switch (date) {
+  case 2:
+    console.log('hom nay la thu 2')
+    break 
+  case 5:
+    console.log('hom nay la thu 5')
+    break
+  case 8:
+    console.log('hom nya la chu nhat')
+    break
+  default:
+    console.log('khong biet')          
 }

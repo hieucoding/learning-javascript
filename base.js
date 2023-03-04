a281a5d1b73302ab58fc07cc239c015750fd9819
unknown <trunghieu210904@gmail.com>
2023-03-04 20:37:30 +0700

    string/array include()

--- base.js
+++ base.js
@@ -1,37 +1,7 @@
-var personID =  [
-  {
-    id: 1,
-    name: 'duong',
-    age: 18
-  },
-  {
-    id: 2,
-    name: 'trung',
-    age: 19
-  },
-  {
-    id: 3,
-    name: 'hieu',
-    age: 20
-  },
-  {
-    id: 4,
-    name: 'dai',
-    age: 22
-  },
-  {
-    id: 5,
-    name: 'nghia',
-    age: 40
-  }
-]
+var newString = 'hoang thanh thang long'
 
-var i = 0
+console.log(newString.includes('hoang'))
 
-function ageTotal (accumulator, currentValue, currentIndex, originalArray) {
-  i++
-  return accumulator + currentValue.age
-}
-var total = personID.reduce(ageTotal, 0)
+var newArray = ['nghin', 'nam', 'van', 'hien']
 
-console.log(total)
+console.log(newArray.includes('nam'))

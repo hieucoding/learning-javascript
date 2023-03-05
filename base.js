1ecbdcac9afa092082d1160d9ec108259d88d381
unknown <trunghieu210904@gmail.com>
2023-03-05 19:24:07 +0700

    math array

--- base.js
+++ base.js
@@ -1,7 +1,15 @@
-var newString = 'hoang thanh thang long'
+console.log(Math.PI) //so pi
 
-console.log(newString.includes('hoang'))
+console.log(Math.round(1.75)) // lam tron
 
-var newArray = ['nghin', 'nam', 'van', 'hien']
+console.log(Math.abs(-4)) //tinh gia tri tuyet doi
 
-console.log(newArray.includes('nam'))
+console.log(Math.ceil(5.000000001)) // lam tron len
+
+console.log(Math.floor(5.9999999)) // lam tron xuong
+
+console.log(Math.random()) // cho mot so random trong khoang tu 0 den 1
+
+console.log(Math.min(-100, 1 , 10, 1000)) // tim so nho nhat
+
+console.log(Math.max(5, 10, 15, 20)) // tim so lon nhat

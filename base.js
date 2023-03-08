0565dd724b12cc87d2152179ccd400abe9cd23f4
unknown <trunghieu210904@gmail.com>
2023-03-08 20:51:44 +0700

    ve hinh tam giac vuong + hinh chu nhat rong

--- base.js
+++ base.js
@@ -1,5 +1,57 @@
-var number = [1, 10, 500, 1989, 12345]
-var i = Math.min(...number)
-var a = Math.max(...number)
-console.log('so nho nhat: ' + i)
-console.log('so lon nhat:' + a)
+// var rows = 5
+
+// for(let a = 1; a <= rows; a++) {
+//     for(let b = 1; b <= a; b++) {
+//         document.write('* ')
+//     } 
+//     document.write('<br/>')
+// }
+
+// for(let a = 1; a <= rows; a++) {
+//     for(let b = a; b <= rows; b++) {
+//         document.write('* ')
+//     }
+//     document.write('<br/>')
+// }
+
+// for(let a = 1; a <= rows; a++) {
+//    for(let b = a; b <= rows; b++){
+//     document.write('&nbsp;&nbsp;')
+//    }
+//    for(let b = 1; b <= a; b++) {
+//     document.write('*')
+//    }
+//    document.write('<br/>')
+// }
+
+// for(let a = 1; a <= rows; a++) {
+//     for(let b = 1; b <= a; b++) {
+//         document.write('&nbsp;&nbsp;')
+//     }
+//     for(let b = a; b <= rows; b++) {
+//         document.write('*')
+//     }
+//     document.write('<br/>')
+// }
+
+// in hinh chu nhat rong
+function veHinh(rows, columns) {
+
+for(a = 1; a <= rows; a++) {
+    if(a == 1 || a == 7 ) {
+        for(let i = 1; i <= 22; i++) {
+            document.write('*')
+        }
+    } else {
+        for(let i = 1; i <= 22; i++){
+            if(i == 1 || i == 22) {
+                document.write('*')
+            } else {
+                document.write('&nbsp;&nbsp;')
+            }
+        }
+    }
+    document.write('<br/>')
+}
+}
+veHinh(7, 22)
